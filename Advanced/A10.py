95b23304f238280d8c85b622844f8f089cc88072
Tomek Prabucki <tom.prabucki@gmail.com>
2016-12-02 16:49:59 +0000

    Comments/Formatting added to A10

--- Advanced/A10.py
+++ Advanced/A10.py
@@ -21,9 +21,12 @@ class List(object):
         self.head.next = self.head
 
     def insert(self, new):
+        ''' Insert new element after the head of list'''
         self.head.next = Node(new, self.head.next)
 
     def display(self, n):
+        ''' Displays next n elements in circle'''
+
         values = []
         old = self.head
         for i in range(n):
@@ -32,6 +35,8 @@ class List(object):
         print("List: ", ",".join(values))
 
     def delete(self, n):
+        ''' Deletes element from circular list'''
+
 
         if n == self.head.value:
             self.head = self.head.next
@@ -39,17 +44,21 @@ class List(object):
         current = self.head.next
         found = False
         size = 0
+
+        # Analyses linked list to determine its size
         while not found:
             if current.value == self.head.value:
                 found = True
             else:
                 current = current.next
             size += 1
-
         current = self.head
-        found = False
+
+        # Searches linked list for node to delete
         while current.value != n:
                 current = current.next
+
+        # Changes link of every node to skip deleted one
         for i in range(size-1):
             current = current.next
         current.next = current.next.next
@@ -57,35 +66,42 @@ class List(object):
             self.head = self.head.next
 
     def play(self, n, Start):
+        '''Simulates the game of children counting each other.
+        Requires list of children, name of child that counts first and returns the winner'''
         current = self.head
         left = n
 
         while current.value != Start:
             current = current.next
 
-        while left > 1:
+        while left > 1: # while there is more than one child in game
+            print('--NEW ROUND--')
             print('Counting: ', current.value)
-            for i in range(n):
+
+            for i in range(n): # count n next children
                 current = current.next
             print('Killing: ', current.value)
-            l.delete(current.value)
-            current = current.next
-            print('WIll count: ', current.value)
-            l.display(4)
+            l.delete(current.value) # delete kid who lost from the list
+            current = current.next # pick next kid to count
+            print('WIll count next: ', current.value)
+            l.display(n) # Shows next n names on list
             left -= 1
             print()
         return current.value
 
 if __name__ == '__main__':
 
+    # Variables
     n = 4
     Names = ['Diana', 'Michael', 'David','Mary']
     Start = 'Diana'
 
-    # I assume next counting person is the one after killed one
-    for i in reversed(range(n)):
-        if i==n-1:
+    # Note: I assume next counting person is the one after killed one (wasn't specified in the task)
+    for i in reversed(range(n)): # Because of the circular list head pointing to end, names are entered in reverse
+        if i == n - 1:  # If this is the first name to add
             l = List(Names[0])
         else:
             l.insert(Names[i+1])
+
+    print()
     print('WINNER: ', l.play(n, Start))
