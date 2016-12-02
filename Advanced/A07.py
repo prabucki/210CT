98087c3df1328b054d3bc3d3b4d2e093206522ae
Tomek Prabucki <tom.prabucki@gmail.com>
2016-12-02 16:10:11 +0000

    Comments/Formatting added to A07

--- Advanced/A07.py
+++ Advanced/A07.py
@@ -10,70 +10,15 @@ person (row, column). Output: a sequence of row/column pairs representing the pe
 successive position.
 '''
 
+# Variables
 n = 12 # Columns
 m = 10  # Rows
 Exit = [7, 10]
 Start = [6, 4]
 
-M = [[0]*n for i in range(m)]
+M = [[0]*n for i in range(m)]   # Creates empty matrix of given size
 
-def PrintMatrix(X):
-    for a in range(len(X)):
-        print("[", end="")
-        for b in range(len(X)):
-            if X[a][b] == 0:
-                print("   ", end="")
-            else:
-                print(X[a][b], " ", end="")
-        print("]")
-    return None
-
-def FindPath(M, Start, Exit):
-    path = []
-    options = [Start.copy()]
-    Current = Start
-
-
-    while Current != Exit and len(options) > 0:
-
-        Found = 0
-
-        # Check directions
-        if (M[Current[0] - 1][Current[1]]) == 1:  # North
-            if ([Current[0] - 1, Current[1]]) not in path:
-                options.append([Current[0] - 1, Current[1]])
-            Found += 1
-        if (M[Current[0]][Current[1] + 1]) == 1:  # East
-            if ([Current[0], Current[1] + 1]) not in path:
-                options.append([Current[0], Current[1] + 1])
-            Found += 1
-        if (M[Current[0] + 1][Current[1]]) == 1:  # South
-            if ([Current[0] + 1, Current[1]]) not in path:
-                options.append([Current[0] + 1, Current[1]])
-            Found += 1
-        if (M[Current[0]][Current[1] - 1]) == 1:  # West
-            if ([Current[0], Current[1] - 1]) not in path:
-                options.append([Current[0], Current[1] - 1])
-            Found += 1
-
-        # Mark dead end
-        if Found == 1 and Current != Start:
-            M[Current[0]][Current[1]] = 0
-            Current = path[-1]
-            del path[-1]
-        else:
-            if Current not in path:
-                path.append(Current)
-            Current = options[-1]
-            del options[-1]
-    if len(options) == 0:
-        print('There is no way to exit')
-    if Current == Exit:
-        print('Path found! Results given in [y][x] format:')
-        path.append(Current)
-    print(path)
-
-
+# Generate matrix (1s are corridors, 0s are walls)
 M[0] = [0,0,0,0,0,0,0,0,0,0,0,0]
 M[1] = [0,1,0,1,0,1,1,1,1,1,0,0]
 M[2] = [0,1,1,1,1,1,0,0,0,1,1,0]
@@ -85,5 +30,84 @@ M[7] = [0,1,1,1,1,0,0,1,0,0,1,0]
 M[8] = [0,1,0,0,1,1,1,1,1,1,1,0]
 M[9] = [0,0,0,0,0,0,0,0,0,0,0,0]
 
+
+def FindPath(M, Start, Exit):
+    '''Finds the path of adjacent 1s in given matrix, from Start field to Exit field'''
+
+    # Start Variables
+    path = []   #
+    options = [Start.copy()] # Discovered ways
+    Current = Start
+
+    while Current != Exit and len(options) > 0:
+
+        Found = 0 # Number of ways found from current location (including return)
+
+        # Look for available ways from current position and add them to available ways
+
+        # Check North
+        if (M[Current[0] - 1][Current[1]]) == 1:
+            if ([Current[0] - 1, Current[1]]) not in path:
+                options.append([Current[0] - 1, Current[1]])
+            Found += 1
+
+        # Check East
+        if (M[Current[0]][Current[1] + 1]) == 1:
+            if ([Current[0], Current[1] + 1]) not in path:
+                options.append([Current[0], Current[1] + 1])
+            Found += 1
+
+        # Check South
+        if (M[Current[0] + 1][Current[1]]) == 1:
+            if ([Current[0] + 1, Current[1]]) not in path:
+                options.append([Current[0] + 1, Current[1]])
+            Found += 1
+
+        # Check West
+        if (M[Current[0]][Current[1] - 1]) == 1:
+            if ([Current[0], Current[1] - 1]) not in path:
+                options.append([Current[0], Current[1] - 1])
+            Found += 1
+
+        # Mark Dead End
+        if Found == 1 and Current != Start: # check if only one way was found (return one)
+            M[Current[0]][Current[1]] = 0 # Mark corridor as wall in the matrix
+            Current = path[-1] # Go back from where you came
+            del path[-1]
+        else:
+            if Current not in path: # We haven't been here yet
+                path.append(Current)    # Add location to our path
+            Current = options[-1]   # Remove current location from available ways
+            del options[-1]
+
+    # Check if we have run out of options:
+    if len(options) == 0:
+        print('There is no way to exit')
+
+    # Check if you have reached the Exit
+    if Current == Exit:
+        print('Path found! Results given in [y][x] format:')
+        path.append(Current)
+    return path
+
+
+def PrintMatrix(X):
+    '''Prints given matrix in a simple visual form'''
+
+    for a in range(len(X)):
+        print("[ ", end="")
+        for b in range(len(X[0])):
+            if a == Exit[0] and b == Exit[1]:
+                print('E  ', end="")
+            elif a == Start[0] and b == Start[1]:
+                print("S  ", end="")
+            elif X[a][b] == 0:
+                print("   ", end="")
+            else:
+                print(X[a][b], " ", end="")
+        print("]")
+
+print()
 PrintMatrix(M)
-FindPath(M, Start, Exit)
+print()
+print(FindPath(M, Start, Exit))
